feb4bf4b2112de4615bf8caadcb6e863fdd4d300
ScuBix1 <bastian.monnin1@gmail.com>
2025-04-17 12:10:42 +0200

    test back ci

--- film-aura-nestjs/src/app.service.ts
+++ film-aura-nestjs/src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    const variable = "toto"
+    const variable = 'toto';
     return 'Hello World!';
   }
 }
